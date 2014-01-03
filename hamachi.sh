50e6af83eee270db8a8757cdf90e448c6bf8e8d6
Robbie <robbie@xcellweb.co.uk>
2014-01-03 10:13:13 +0000

    new file:   .gitattributes 	new file:   .gitignore 	new file:   hamachi.sh

---
+++ hamachi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "installing requirements"
+sudo apt-get update
+sudo apt-get -y install --fix-missing lsb lsb-core
+echo "Downloading Hamachi"
+sudo wget https://www.dropbox.com/s/ectspm1x1lno4sh/logmein-hamachi-2.1.0.101-armel.tgz
+echo "extracting Hamachi"
+tar xzvf logmein-hamachi-2.1.0.101-armel.tgz
+cd log*
+echo "installing hamachi"
+sudo ./install.sh
+/etc/init.d/logmein-hamachi start
+echo "installing chkconfig"
+sudo apt-get -y install chkconfig
+sudo  chkconfig -s logmein-hamachi 2
+echo "starting hamachi"
+sudo hamachi login
+echo "please enter your logmein account email address"
+read email
+sudo hamachi attach $email
+echo "please enter your raspberry pi nickname "
+read nick
+sudo hamachi set-nick $nick
+echo "your pi should now be on your hamachi network"
